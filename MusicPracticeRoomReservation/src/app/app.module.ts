2cf2fb8dcdccc61075bcf14b2542f0ec94d60cab
Methee <FongsabuuYo@gmail.com>
2019-04-08 12:09:06 +0700

    add ng-bootstrap, bootstrap

--- MusicPracticeRoomReservation/src/app/app.module.ts
+++ MusicPracticeRoomReservation/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//ng-bootstrap
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
